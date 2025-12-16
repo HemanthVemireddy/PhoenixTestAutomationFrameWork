06002c40cc6f6289875502bb434ff979686b8a28
Hemanth Kumar Reddy <150761370+HemanthVemireddy@users.noreply.github.com>
2025-12-16 09:37:27 +0530

    Base code for my automation framework

---
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -0,0 +1,48 @@
+package com.api.tests;
+
+import static io.restassured.RestAssured.*;
+
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+import com.api.pojo.UserCredentials;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class LoginAPITest {
+	
+	@Test
+	public void loginAPITest()
+	{
+
+		UserCredentials user = new UserCredentials("iamfd","password");
+		
+		given()
+			.baseUri("http://64.227.160.186:9000/v1")
+		.and()
+			.contentType(ContentType.JSON)
+		.and()
+			.accept(ContentType.JSON)
+		.and()
+			.body(user)
+		.and()
+			.log().uri()
+			.log().method()
+			.log().headers()
+			.log().body()
+		
+		.when()
+			.post("login")
+		.then()
+			.statusCode(200)
+			.time(lessThan(1000L))
+			.log().all()
+		    .body("message", equalTo("Success"))
+		    .and()
+		    .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema\\LoginResponseSchema.json"));
+		
+	}
+
+}
